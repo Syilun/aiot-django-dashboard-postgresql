a908826dc318cde8d55d1e5dfd9ae7d8497e7d50
Syilun <allen.shaw129@gmail.com>
2020-09-10 06:14:07 +0800

    connected to rds

---
+++ app/models.py
@@ -0,0 +1,20 @@
+# -*- encoding: utf-8 -*-
+"""
+License: MIT
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator
+
+# Create your models here.
+class PERSON(models.Model):
+    time = models.DateTimeField(auto_now_add=True)
+    in_out = models.BooleanField()
+
+
+class FACE(models.Model):
+    time = models.DateTimeField(auto_now_add=True)
+    gender = models.BooleanField()
+    age = models.PositiveSmallIntegerField(validators=[MaxValueValidator(7)])
